374fe4bf3bf7c9992625a8f64c7a43694b84f735
Ubuntu <ubuntu@siu-notebook.c.dym-ap-siu-habitat.internal>
2019-11-18 17:52:54 +0000

    Set password before starting Jupyter notebook

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+jupyter notebook --generate-config
+
+echo
+echo "** Please enter a password for your Jupyter Notebook. **"
+echo
+jupyter notebook password
 
 # Iniciando jupyter notebook
 #jupyter notebook --ip=0.0.0.0 --no-browser
